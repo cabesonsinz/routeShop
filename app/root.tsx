344c60589ad5ab6acfb84cc23a58250fd07d6258
cabesonsinz <cabesonsinz@gmail.com>
2025-08-14 00:29:46 -0400

    details-ready

--- app/root.tsx
+++ app/root.tsx
@@ -49,8 +49,8 @@ export default function App() {
   return( 
     <>
       <header className="CABESONXD">
-         <Link to={"products"}>PRODUCTS</Link> 
-         <Link to={"cart"}>CART</Link>
+         <Link to={"products"}><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#ffffff"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-building-store"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l18 0" /><path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" /><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" /></svg></Link> 
+         <Link to={"cart"}><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#ffffff"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg></Link>
       </header>
 
 
--- app/routes/productDetail.tsx
+++ app/routes/productDetail.tsx
@@ -1,33 +1,41 @@
 
 import type { Route } from "../+types/root";
 
-
-
-export async function clientLoader({params}:Route.ClientLoaderArgs) {
-
-    const res = await fetch(`https://fakestoreapi.com/products/${params.id}`)
-    const product = await res.json()
-    return product
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+    const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+    const product = await res.json();
+    return product;
 }
 
-
-
-export default function productDetails({loaderData}:Route.ComponentProps){
+export default function productDetails({ loaderData }: Route.ComponentProps) {
     if (!loaderData) {
         return <div>Loading...</div>;
     }
-    const {category,description,rating,image,price,title} =  loaderData;
+    const { category, description, rating, image, price, title } = loaderData;
+    const {count,rate} = rating
 
-    return(
+
+
+    return (
         <main>
-            <section className="productos">
+            <section className="productDetail">
+                <img src={image} alt={description} />
 
-                <div className="producto">
-                        <img src={image} alt="" />
-                        
-                </div>
-
+                <aside className="productDescription">
+                    <h1>{title}</h1>
+                    <p>{category}</p>
+                    <p>{description}</p>
+                    <div className="productMinData">
+                        <p>PRICE: {price}</p>
+                        <p>CALIFICATION <b>{count} </b> SCORE <b>{rate}</b></p>
+                    </div>
+                    <section className="addButtons">
+                        <button className="addCart">ADD CART</button>
+                        <button className="buy">BUY</button>
+                    </section>
+                </aside>
             </section>
+
         </main>
-    )
-} 
+    );
+}
